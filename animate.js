7c3ce0ea2b68027cae08a04cda7e5eb9f3e4f998
Bonnie <57296407+bon988@users.noreply.github.com>
2021-04-26 14:18:50 +0100

    initial commit

---
+++ animate.js
@@ -0,0 +1,22 @@
+// darken navigation on scroll 
+$(window).scroll(function(){
+    if($(window).scrollTop()){
+        $("nav").addClass("dark");
+    }
+    else{
+        $("nav").removeClass("dark");
+    }
+});
+
+// JavaScript for label effects only
+$(window).load(function(){
+    $(".border-animation input").val("");
+    
+    $(".input-effect input").focusout(function(){
+      if($(this).val() != ""){
+        $(this).addClass("has-content");
+      }else{
+        $(this).removeClass("has-content");
+      }
+    })
+  });
